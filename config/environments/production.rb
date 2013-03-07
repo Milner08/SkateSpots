a979f4a330c5fa00a9b5c9f6fd4cc2fe9a55aec8
Tomas Milner <tomas@themilners.net>
2013-03-07 12:46:07 +0000

    Dont force SSL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Skate::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
-  config.force_ssl = true
+  config.force_ssl = false
 
   # Code is not reloaded between requests
   config.cache_classes = true
